d3d1c5f8bb16ad008937adb6dc38fc7a4d362d08
marinapleguezuelos <marinapleguezuelos14@gmail.com>
2017-02-09 10:02:20 +0100

    Fix Bug

--- public/javascripts/goalController.js
+++ public/javascripts/goalController.js
@@ -1,14 +1,21 @@
 angular.module('goalController', [])
 .controller('goalCtrl', function($scope, $http, Goals) {
   Goals.get().success(function(data) {
+  for (var i=0; i<data.length;i++){
+      data[i].ok=i;
+      data[i].ko=1;
+      data[i].percentage= (data[i].ok/(data[i].ko+data[i].ko))*100
+    }
     $scope.goals = data;
   });
   $scope.addGoal = function() {
     if(!$scope.goalName || $scope.goalName === '') { return; }
     var goalObject = {
-      description: $scope.goalName
+      description: $scope.goalName,
     };
     Goals.create(goalObject).success(function(data) {
+      goalObject._id=data._id;
+      goalObject.percentage=11;
       $scope.goals.push(goalObject);
       $scope.goalName = '';
     });

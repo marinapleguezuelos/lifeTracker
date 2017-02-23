6fa5126cc678cf314aa979ab27e24cd8abe7ffc5
marinapleguezuelos <marinapleguezuelos14@gmail.com>
2017-02-23 09:43:02 +0100

    bugs fixed

--- public/javascripts/goalController.js
+++ public/javascripts/goalController.js
@@ -1,16 +1,9 @@
 angular.module('goalController', [])
 .controller('goalCtrl', function($scope, $http, Goals) {
   Goals.get().success(function(data) {
-<<<<<<< HEAD
-  for (var i=0; i<data.length;i++){
-      data[i].ok=i;
-      data[i].ko=1;
-      data[i].percentage= (data[i].ok/(data[i].ko+data[i].ko))*100
-=======
     for (var currentIndex=0; currentIndex<data.length; currentIndex=currentIndex+1) {
       var currentElement = data[currentIndex];
       currentElement.percentage = (currentElement.ok/(currentElement.ok+currentElement.ko))*100;
->>>>>>> profjesus/master
     }
     $scope.goals = data;
   });
@@ -18,20 +11,12 @@ angular.module('goalController', [])
     if(!$scope.goalName || $scope.goalName === '') { return; }
     var goalObject = {
       description: $scope.goalName,
-<<<<<<< HEAD
-    };
-    Goals.create(goalObject).success(function(data) {
-      goalObject._id=data._id;
-      goalObject.percentage=11;
-      $scope.goals.push(goalObject);
-=======
       ok: 0,
       ko: 0
     };
     Goals.create(goalObject).success(function(data) {
       data.percentage = 0;
       $scope.goals.push(data);
->>>>>>> profjesus/master
       $scope.goalName = '';
     });
   };
